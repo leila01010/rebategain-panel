7c23a3a59d8fdaeba2dcf4a7514a434f99d8c248
Leila Ahmadi <leila1ahmadi2@gmail.com>
2026-04-30 10:57:54 +0330

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { computed } from 'vue'
+import { RouterView, useRoute } from 'vue-router'
+import BlankLayout from '@/layouts/blankLayout.vue'
+import DefaultLayout from '@/layouts/defaultLayout.vue'
+
+const route = useRoute()
+
+const layouts = {
+  default: DefaultLayout,
+  blank: BlankLayout,
+}
+
+const layout = computed(() => layouts[route.meta.layout] || DefaultLayout)
+</script>
+
+<template>
+  <component :is="layout">
+    <RouterView />
+  </component>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <div class="text-primary text-3xl">Home View</div>
+</template>
