19e88003ee11ad42d90e2dbbc532ccfa133e2308
Leila Ahmadi <leila1ahmadi2@gmail.com>
2026-05-10 17:16:22 +0330

    feat: add IrCard component

--- src/lib/ui-kit/index.js
+++ src/lib/ui-kit/index.js
@@ -1,8 +1,10 @@
 import IrIcon from '@/lib/ui-kit/packages/IrIcon/IrIcon.vue'
 import IrButton from '@/lib/ui-kit/packages/IrButton/IrButton.vue'
+import IrCard from './packages/IrCard/IrCard.vue'
 
 
 export {
   IrIcon,
   IrButton,
+  IrCard,
 }
---
+++ src/lib/ui-kit/packages/IrCard/IrCard.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+  title: { type: String, default: '' },
+  subtitle: { type: String, default: '' },
+  flush: { type: Boolean, default: false },
+  headerClass: { type: String, default: '' },
+  bodyClass: { type: String, default: '' },
+  footerClass: { type: String, default: '' },
+})
+</script>
+
+<template>
+  <div :class="['ir-card', { 'ir-card--flush': flush }]">
+    <div v-if="$slots.header || title" :class="['ir-card__header', headerClass]">
+      <div class="ir-card__header-start">
+        <div class="ir-card__meta">
+          <h3 v-if="title" class="ir-card__title">{{ title }}</h3>
+          <p v-if="subtitle" class="ir-card__subtitle">{{ subtitle }}</p>
+        </div>
+      </div>
+
+      <div class="ir-card__header-end">
+        <slot name="header-actions" />
+      </div>
+    </div>
+    <div :class="['ir-card__body', bodyClass]">
+      <slot />
+    </div>
+    <div v-if="$slots.footer" :class="['ir-ir-card__footer', footerClass]">
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" src="./IrCard.scss" />
