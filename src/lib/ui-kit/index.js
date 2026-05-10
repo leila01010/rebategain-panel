b6326a491a8c6702e88d063381910055ba4a02d9
Leila Ahmadi <leila1ahmadi2@gmail.com>
2026-05-10 17:18:24 +0330

    feat: add IrChip component

--- src/lib/ui-kit/index.js
+++ src/lib/ui-kit/index.js
@@ -1,10 +1,12 @@
 import IrIcon from '@/lib/ui-kit/packages/IrIcon/IrIcon.vue'
 import IrButton from '@/lib/ui-kit/packages/IrButton/IrButton.vue'
 import IrCard from './packages/IrCard/IrCard.vue'
+import IrChip from './packages/IrChip/IrChip.vue'
 
 
 export {
   IrIcon,
   IrButton,
   IrCard,
+  IrChip,
 }
