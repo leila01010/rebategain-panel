9accc5f3512d6235f434c687a42b9675854760dd
Leila Ahmadi <leila1ahmadi2@gmail.com>
2026-05-10 17:51:12 +0330

    feat: add IrInput component

--- src/lib/ui-kit/index.js
+++ src/lib/ui-kit/index.js
@@ -1,9 +1,10 @@
-import IrIcon from '@/lib/ui-kit/packages/IrIcon/IrIcon.vue'
-import IrButton from '@/lib/ui-kit/packages/IrButton/IrButton.vue'
+import IrIcon from './packages/IrIcon/IrIcon.vue'
+import IrButton from './packages/IrButton/IrButton.vue'
 import IrCard from './packages/IrCard/IrCard.vue'
 import IrChip from './packages/IrChip/IrChip.vue'
 import IrModal from './packages/IrModal/IrModal.vue'
 import IrTable from './packages/IrTable/IrTable.vue'
+import IrInput from './packages/IrInput/IrInput.vue'
 
 
 export {
@@ -13,4 +14,5 @@ export {
   IrChip,
   IrModal,
   IrTable,
+  IrInput,
 }
