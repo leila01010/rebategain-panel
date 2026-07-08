96f77ecb5471b9b7f506e9a5206eeda44446dbec
Leila Ahmadi <leila1ahmadi2@gmail.com>
2026-07-08 13:38:33 +0330

    feat: link contact support to mail

--- src/components/layout/AppSidebar.vue
+++ src/components/layout/AppSidebar.vue
@@ -103,6 +103,8 @@ const sidebarStyle = computed(() => props.zIndex ? { zIndex: props.zIndex } : nu
         size="lg"
         prepend-icon="mail"
         block
+        tag="a"
+        url="mailto:Support@rebategain.com"
         class="!border-dark-blue-50 !text-dark-blue-500"
       />
     </div>
